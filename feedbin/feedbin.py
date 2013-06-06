569891bc14390f8454081b397fa926eb74bc0dd8
j.c.sackett <j.c.sackett@gmail.com>
2013-06-05 20:42:31 -0400

    Initial commit.
    * Basic layout
    * Setup file
    * Gitignore

---
+++ feedbin/feedbin.py
@@ -0,0 +1,2 @@
+class Feedbin:
+    pass
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(name='feedbin',
+    version='0.1',
+    description='Python3 library for the feedbin API',
+    url='http://github.com/jaycee/python-feedbin',
+    author='j.c.sackett',
+    author_email='j.c.sackett@gmail.com',
+    license='BSD',
+    packages=['feedbin'],
+    categories=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License'
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        ]
+    )
