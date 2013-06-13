72293ed254859d03d6c9ee511704974a8b917c29
j.c.sackett <j.c.sackett@gmail.com>
2013-06-13 08:43:00 -0400

    Better tests * Added tests for requests with params. * Split auth header test from basic request url test.

--- feedbin/tests/test_feedbin.py
+++ feedbin/tests/test_feedbin.py
@@ -14,14 +14,24 @@ class FeedBinTestCase(TestCase):
         self.assertEqual(b'Basic', scheme)
         self.assertEqual(b'jsmith@example.com:secret', b64decode(auth_string))
 
-    def test_requests_without_params(self):
+    def test_requests_have_auth_header(self):
         f = Feedbin('jsmith@example.com','secret')
         req = f._makeRequest('entries')
         self.assertIn('Authorization', req.headers.keys())
+
+    def test_requests_without_params(self):
+        f = Feedbin('jsmith@example.com','secret')
+        req = f._makeRequest('entries')
         self.assertEqual(
             'http://api.feedbin.me/v2/entries.json',
             req.get_full_url())
-    
+
+    def test_requests_with_params(self):
+        f = Feedbin('jsmith@example.com','secret')
+        req = f._makeRequest('entries', {'foo': 'bar'})
+        self.assertEqual(
+            'http://api.feedbin.me/v2/entries.json?foo=bar',
+            req.get_full_url())
 
 if __name__ == '__main__':
     main() 
